298a59c8da90492b366ec99dbd7b3af9910e1f5f
ZloyNick <ravil.parkour@gmail.com>
2021-02-16 16:32:39 +0500

    Update CompaniesController.php

--- app/Api/v1/CompaniesController.php
+++ app/Api/v1/CompaniesController.php
@@ -1,28 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Api\v1;
 
 use App\Company;
 use Illuminate\Http\Request;
 
+use function response, strlen;
+
 class CompaniesController
 {
-
+    
     function getCompanies(Request $request, string $inn)
     {
-        if(!$inn)
-        {
-            return response()->json(['message' => 'Empty company\'s INN given'], 422);
-        }
-
-        if(strlen($inn) != 10)
-        {
-            return response()->json(['message' => 'Invalid company\'s INN given'], 422);
-        }
-
-        $companies = Company::with('owner')->where('inn', $inn)->get();
-
-        return response()->json(['companies' => $companies]);
+       return !$inn ? ($resp = response())->json(['message' => 'Empty company\'s INN given'], 422)
+           : (!strlen($inn) != 10 ? $resp->json(['message' => 'Empty company\'s INN given'], 422)
+             : $resp->json(['companies' => Company::with('owner')->where('inn', $inn)->get()]);
     }
 
 }
